793b6e1a62d5f2956924a223d7b3f5668d221f02
Malgorzata Rog <Malgorzata.Rog@softwire.com>
2021-06-29 16:36:25 +0100

    Adding function addWordAtStartForIteratorDivisibleBy that adds given word at the beggining of the expression for numbers divisible by given divisor. Applied for word Fezz and divisor 13.

--- src/malrog/fizzbuzz/Main.java
+++ src/malrog/fizzbuzz/Main.java
@@ -5,13 +5,14 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int maxValue = 100;
+        int maxValue = 200;
         for (int i = 1; i <= maxValue; i++) {
             List<String> words = new ArrayList<>();
             addWordAtEndForIteratorDivisibleBy(i, 3, "Fizz", words);
             addWordAtEndForIteratorDivisibleBy(i, 5, "Buzz", words);
             addWordAtEndForIteratorDivisibleBy(i, 7, "Bang", words);
             substituteWordForIteratorDivisibleBy(i, 11, "Bong", words);
+            addWordAtStartForIteratorDivisibleBy(i, 13, "Fezz", words);
             if (words.isEmpty()) System.out.print(i);
             else for (String word : words) System.out.print(word);
             System.out.print("\n");
@@ -26,5 +27,9 @@ public class Main {
             words.clear();
             words.add(wordToAdd);
         }
+    }
+
+    static void addWordAtStartForIteratorDivisibleBy(int iterator, int divisor, String wordToAdd, List<String> words) {
+        if (iterator % divisor == 0) words.add(0, wordToAdd);
     }
 }
