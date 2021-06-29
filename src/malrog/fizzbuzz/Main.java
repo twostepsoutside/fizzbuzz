6e4e970e76200efaa76278c97f5b903314fc9eae
Malgorzata Rog <Malgorzata.Rog@softwire.com>
2021-06-29 16:46:23 +0100

    Adding function reverseOrderForIteratorDivisibleBy that reverses existing words for numbers divisible by given divisor. Applied for divisor 17. Range of numbers changed to 1-300.

--- src/malrog/fizzbuzz/Main.java
+++ src/malrog/fizzbuzz/Main.java
@@ -1,11 +1,12 @@
 package malrog.fizzbuzz;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int maxValue = 200;
+        int maxValue = 300;
         for (int i = 1; i <= maxValue; i++) {
             List<String> words = new ArrayList<>();
             addWordAtEndForIteratorDivisibleBy(i, 3, "Fizz", words);
@@ -13,6 +14,7 @@ public class Main {
             addWordAtEndForIteratorDivisibleBy(i, 7, "Bang", words);
             substituteWordForIteratorDivisibleBy(i, 11, "Bong", words);
             addWordAtStartForIteratorDivisibleBy(i, 13, "Fezz", words);
+            reverseOrderForIteratorDivisibleBy(i,17, words);
             if (words.isEmpty()) System.out.print(i);
             else for (String word : words) System.out.print(word);
             System.out.print("\n");
@@ -22,14 +24,18 @@ public class Main {
     static void addWordAtEndForIteratorDivisibleBy(int iterator, int divisor, String wordToAdd, List<String> words) {
         if (iterator % divisor == 0) words.add(wordToAdd);
     }
-    static void substituteWordForIteratorDivisibleBy(int iterator, int divisor, String wordToAdd, List<String> words) {
+    static void substituteWordForIteratorDivisibleBy(int iterator, int divisor, String wordToSubstitute, List<String> words) {
         if (iterator % divisor == 0){
             words.clear();
-            words.add(wordToAdd);
+            words.add(wordToSubstitute);
         }
     }
 
     static void addWordAtStartForIteratorDivisibleBy(int iterator, int divisor, String wordToAdd, List<String> words) {
         if (iterator % divisor == 0) words.add(0, wordToAdd);
+    }
+
+    static void reverseOrderForIteratorDivisibleBy(int iterator, int divisor, List<String> words) {
+        if (iterator % divisor == 0) Collections.reverse(words);
     }
 }
