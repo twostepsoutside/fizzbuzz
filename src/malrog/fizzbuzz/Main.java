4c7ef1f58d5176d9c706362a9f4b37d5158d3b2b
Malgorzata Rog <Malgorzata.Rog@softwire.com>
2021-06-29 16:10:24 +0100

    Previous rules are executed by a use of an outside function addWordAtEndForIteratorDivisibleBy.

--- src/malrog/fizzbuzz/Main.java
+++ src/malrog/fizzbuzz/Main.java
@@ -1,12 +1,22 @@
 package malrog.fizzbuzz;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
-            if (i % 3 == 0) System.out.print("Fizz");
-            if (i % 5 == 0) System.out.print("Buzz");
-            if (i % 3 != 0 && i % 5 != 0) System.out.print(i);
+        int maxValue = 100;
+        for (int i = 1; i <= maxValue; i++) {
+            List<String> words = new ArrayList<>();
+            addWordAtEndForIteratorDivisibleBy(i, 3, "Fizz", words);
+            addWordAtEndForIteratorDivisibleBy(i, 5, "Buzz", words);
+            if (words.isEmpty()) System.out.print(i);
+            else for (String word : words) System.out.print(word);
             System.out.print("\n");
         }
+    }
+
+    static void addWordAtEndForIteratorDivisibleBy(int iterator, int divisor, String wordToAdd, List<String> words) {
+        if (iterator % divisor == 0) words.add(wordToAdd);
     }
 }
